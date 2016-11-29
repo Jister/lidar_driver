e5fafc8db24b6ee6220a7be8758e1dfe9ed3ef7a
Jister <chenjiesjtu14@gmail.com>
2016-11-29 18:47:28 +0800

    update leddar

--- src/leddar_one/leddar_node.cpp
+++ src/leddar_one/leddar_node.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 #include "leddar_one/LeddarOne.h"
 
@@ -11,10 +12,24 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "leddar_node");
 	ros::NodeHandle n;
-	ros::Publisher pub = n.advertise<lidar_driver::Lidar>("/leddar", 1000);
+	ros::NodeHandle n_private("~");
+	ros::Publisher pub = n.advertise<lidar_driver::Lidar>("/lidar", 1000);
 	ros::Rate loop_rate(20);
 
-	char sPortName[LT_MAX_PORT_NAME_LEN] = "ttyUSB0";
+	std::string device;
+
+	if (n_private.getParam("device", device))
+	{
+		ROS_INFO("Device set to: %s", device.c_str());
+	}
+	else
+	{
+		device = "/dev/ttyUSB0";  
+		ROS_INFO("Using the default USB device: /dev/ttyUSB0");
+	}
+
+	char sPortName[LT_MAX_PORT_NAME_LEN];
+	strcpy(sPortName, device.c_str());
 	unsigned int uBaudRate = 115200;
 	unsigned int uParity = LT_PARITY_NONE;
 	unsigned int uStopBits = 1;
@@ -39,7 +54,7 @@ int main(int argc, char **argv)
 					msg.distance.data = lDetections[i].mDistance;
 					msg.amplitude.data = lDetections[i].mAmplitude;
 					msg.temperature.data = lAcquisition.mTemperature;
-					ROS_INFO("distance:%f",lDetections[i].mDistance);
+					//ROS_INFO("distance:%f",lDetections[i].mDistance);
 					pub.publish(msg);
 				}
 			}else
@@ -50,7 +65,7 @@ int main(int argc, char **argv)
 			loop_rate.sleep();
 		}
 	}else{
-		printf("Connection failed (error %i)!\n", (int)result);
+		ROS_INFO("Connection failed (error %i)!\n", (int)result);
 	}
 
 	return 0;
