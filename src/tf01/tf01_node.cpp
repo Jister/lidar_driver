597f0317f0a1182b022bb49595df7d937319b681
Jister <chenjiesjtu14@gmail.com>
2017-08-08 19:04:28 +0800

    modify

--- src/tf01/tf01_node.cpp
+++ src/tf01/tf01_node.cpp
@@ -1,20 +1,28 @@
 #include "tf01/tf01.h"
 #include "lidar_driver/Lidar.h"
+#include <sensor_msgs/Range.h>
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "tf01_node");
 	ros::NodeHandle n;
 	ros::NodeHandle n_private("~");
-	ros::Publisher lidar_pub = n.advertise<lidar_driver::Lidar>("/lidar", 10);
+	ros::Publisher lidar_pub = n.advertise<sensor_msgs::Range>("/mavros/lidar/range", 10);
 	TF01 tf01(n_private);
 	ros::Rate loop_rate(500);
 
 	bool first_measure = false;
-	lidar_driver::Lidar msg;
-	
+	//lidar_driver::Lidar msg;
+	sensor_msgs::Range msg;	
+	int cnt = 0;
 	while(ros::ok())
 	{
+		cnt++;
+		if(cnt>10)
+		{
+			cnt = 0;
+		}
+
 		tf01.read_data();
 
 		if(tf01.data_valid)
@@ -25,13 +33,19 @@ int main(int argc, char **argv)
 				first_measure = true;
 			}
 
+			//msg.header.stamp = ros::Time::now();
+			//msg.distance.data = tf01.distance / 100.0;
+			//msg.amplitude.data = tf01.amplitude;
 			msg.header.stamp = ros::Time::now();
-			msg.distance.data = tf01.distance / 100.0;
-			msg.amplitude.data = tf01.amplitude;
-			lidar_pub.publish(msg);
-
+			msg.radiation_type = sensor_msgs::Range::INFRARED;
+			msg.min_range = 0.1;
+			msg.max_range = 10.0;
+			msg.range = tf01.distance / 100.0;
+			if(cnt == 0)
+			{
+				lidar_pub.publish(msg);
+			}
 			tf01.data_valid = false;
-			
 		}
 		ros::spinOnce();
 		loop_rate.sleep();
